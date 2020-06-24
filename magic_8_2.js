ca5cb427c65a7578cf30521d9b264a214bc9c7e7
Danial Jalaluddin <dan90@live.com.my>
2020-06-25 01:09:55 +0800

    Update magic_8_2.js

--- magic_8_2.js
+++ magic_8_2.js
@@ -1,29 +1,38 @@
 let userName = '';
 
-userName ? console.log(`Hello, ${userName}!`):console.log('Hello!');
+userName ? console.log(`Hello {userName}`):console.log('Hello');
 
-const userQuestion = 'What\'s the lucky no. ?';
+let userQuestion = 'Should I order pizza ?';
 
-console.log(`The Magic ball's ask: ${userQuestion}`);
+if(userName){
+    console.log(`Hi I\'m ${userName} ${userQuestion}`);
+} else {
+    console.log(`Hi, ${userQuestion}`);
+}
 
 let randomNumber = Math.floor(Math.random() * 8);
 
-let eigthball ;
+let eightBall='';
 
 if(randomNumber === 0){
-    eigthball = 'It is certain';
+    eightBall = 'It is certain';
 } else if (randomNumber === 1){
-    eigthball = 'Reply hazy try again';
+    eightBall = 'It is decidely so';
 } else if (randomNumber === 2){
-    eigthball = 'Cannot predict now';
+    eightBall = 'Reply hazy try again';
 } else if (randomNumber === 3){
-    eigthball = 'Do not count on it';
+    eightBall = 'Cannot predict now';
 } else if (randomNumber === 4){
-    eigthball = 'My sources say no';
+    eightBall = 'Do not count on it';
 } else if (randomNumber === 5){
-    eigthball = 'Outlook not so good';
+    eightBall = 'My sources say no';
 } else if (randomNumber === 6){
-    eigthball = 'Signs point to yes';
+    eightBall = 'Outlook not so good';
+} else if (randomNumber === 7){
+    eightBall = 'Signs point to yes';
 }
 
-console.log(`The magic ball answer ${eigthball}`);
+console.log(eightBall);
+
+
+
